ad562d82d776a341716c724083325add9485da09
sunil <sunilbdrbist@gmail.com>
2019-05-04 19:40:36 +0545

    added email address and generated getters and setters

--- app/src/main/java/np/com/westernit/onlineaccount/entity/Student.java
+++ app/src/main/java/np/com/westernit/onlineaccount/entity/Student.java
@@ -3,7 +3,7 @@ package np.com.westernit.onlineaccount.entity;
 public class Student {
     private Integer roll;
     private String name;
-   // private String anotherVariable;
+    private String email;
 
     public Student(Integer roll, String name) {
         this.roll = roll;
@@ -25,5 +25,13 @@ public class Student {
 
     public void setName(String name) {
         this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
     }
 }
